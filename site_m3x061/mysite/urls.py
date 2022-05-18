c7f795e43a1b324a1ad12a4ddc0f0f44ea0614aa
maria <ma.usachova@gmail.com>
2022-05-18 13:49:16 +0300

    5-end

--- site_m3x061/mysite/urls.py
+++ site_m3x061/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, SubjectDetailView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('blog/<slug>/', SubjectDetailView.as_view(), name='subject_detail'),
 ]
--- site_m3x061/mysite/views.py
+++ site_m3x061/mysite/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Subject
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        subjects = Subject.objects.all()
+        subject = Subject.objects.all()
         return render(
             request,
             'mysite/home.html',
             context={
-                'subjects': subjects
+                'subject': subject
             }
 
         )
+
+class SubjectDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        subject = get_object_or_404(Subject, url=slug)
+        return render(request, 'mysite/subject_detail.html', context={
+            'subject': subject
+    })
