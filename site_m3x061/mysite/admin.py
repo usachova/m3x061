a582a7884836cc705df8a1090db68cdc41e406c5
maria <ma.usachova@gmail.com>
2022-07-05 02:07:20 +0300

    add teacher

--- site_m3x061/mysite/admin.py
+++ site_m3x061/mysite/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from mysite.models import Subject, Student
+from mysite.models import Subject, Student, Teacher
 
 
 class SubjectAdmin(admin.ModelAdmin):
     pass
-
 admin.site.register(Subject, SubjectAdmin)
 
 class StudentAdmin(admin.ModelAdmin):
     pass
-
 admin.site.register(Student, StudentAdmin)
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Teacher, TeacherAdmin)
--- site_m3x061/mysite/models.py
+++ site_m3x061/mysite/models.py
@@ -3,17 +3,22 @@ from django.db import models
 # from django.contrib.auth.models import User
 # from django.utils import timezone
 
+class Teacher(models.Model):
+    surname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    patronymic = models.CharField(max_length=200, null=True, blank=True)
+    isu = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        patronymic = self.patronymic if self.patronymic else ''
+        return self.surname + ' ' + self.name + ' ' + patronymic
 
 class Subject(models.Model):
     slug = models.CharField(max_length=25)
     title = models.CharField(max_length=200)
     сontrol_type = models.CharField(max_length=200)
-    # lector = models.CharField(max_length=200)
-    # lector_isu = models.CharField(max_length=200, null=True, blank=True)
-    # content_lectures = models.TextField()
-    # practice = models.CharField(max_length=200)
-    # practice_isu = models.CharField(max_length=200, null=True, blank=True)
-    # content_practice = models.TextField()
+    # lector = models.ForeignKey(Teacher, on_delete=models.SET_NULL, blank=True, null=True, related_name='lector')
+    # practice = models.ForeignKey(Teacher, on_delete=models.SET_NULL, blank=True, null=True, related_name='practice')
     # deadlines = models.TextField()
 
     def __str__(self):
@@ -27,3 +32,5 @@ class Student(models.Model):
     def __str__(self):
         patronymic = self.patronymic if self.patronymic else ''
         return self.surname + ' ' + self.name + ' ' + patronymic
+
+
